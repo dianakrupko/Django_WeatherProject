91dde0d62fa46e208ad8b238000b04e48fa6d7b8
dianakrupko <90472816+dianakrupko@users.noreply.github.com>
2022-02-15 20:24:42 +0200

    new files

--- weatherproject/main/urls.py
+++ weatherproject/main/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="home"),
+    path('authorization', views.authorization, name="autorization"),
+    path('news', views.about, name="news"),
     path('about', views.about, name="about"),
-    path('news',views.news)
+    path('contacts',views.news,name="contacts")
 ]
--- weatherproject/main/views.py
+++ weatherproject/main/views.py
@@ -2,15 +2,21 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-# Create your views here.
-
 def index(request):
-    return render(request,'main/index.html')
+    return render(request, 'main/index.html')
 
 
-def about(request):
-    return render(request,'main/about.html')
+def authorization(request):
+    return render(request, 'main/authorization.html')
 
 
 def news(request):
-    return HttpResponse("<h4>Новини</h4>")
+    return render(request, 'main/news.html')
+
+
+def about(request):
+    return render(request, 'main/about.html')
+
+
+def contacts(request):
+    return render(request, 'main/contacts.html')
