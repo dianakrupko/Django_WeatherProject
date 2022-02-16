ae640216938dd2c2d2c08bc18e996130a7c206d7
dianakrupko <90472816+dianakrupko@users.noreply.github.com>
2022-02-16 04:47:46 +0200

    changes

--- weatherproject/data/urls.py
+++ weatherproject/data/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<str:month>', views.data_home, name="data_home"),
+    path('my/<str:month>', views.data_home, name="data_home"),
     path('index/<str:inter>', views.data_interpolate, name="data_interpolate"),
     path('index', views.data_index, name="data_index"),
     path('int', views.data_interpolate, name="data_index1"),
