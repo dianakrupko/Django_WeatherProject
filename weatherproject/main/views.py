675cf567d8b22a7b451e3a8a5ceb02da86fd0135
dianakrupko <90472816+dianakrupko@users.noreply.github.com>
2022-02-19 00:23:19 +0200

    .

--- weatherproject/main/views.py
+++ weatherproject/main/views.py
@@ -1,7 +1,8 @@
 import requests
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import City
+from .formsWheatherCity import CityForm
 
 def index(request):
     return render(request, 'main/index.html')
@@ -26,18 +27,33 @@ def contacts(request):
 def weatherCity(request):
     appid = '05055e51f5b5c15967f1cfb21c3d3a55'
     url = 'https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=' + appid
-    city = "London"
 
-    res = requests.get(url.format(city)).json()
+    if(request.method == 'POST'):
+        form=CityForm(request.POST)
+        form.save()
+
+    form = CityForm()
 
 
-    city_info={
-        'city': city,
-        'temp':res['main']['temp'],
-        # 'icon':res['weather'][0]['icon']
-    }
+    cities = City.objects.all()
+
+    all_cities=[]
+
+    for city in cities:
+        res = requests.get(url.format(city.name)).json()
+        city_info={
+            'city': city.name,
+            'temp':res['main']['temp'],
+            'pressure':res['main']['pressure'],
+            'humidity':res['main']['humidity'],
+            'speed':res['wind']['speed'],
+            'icon':res['weather'][0]['icon']
+        }
+        all_cities.append(city_info)
+
     contex={
-        'info':city_info
+        'all_info': all_cities,
+        'form':form
     }
 
     return render(request, 'main/weatherCity.html', contex)
