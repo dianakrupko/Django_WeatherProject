116a8c57148aeb69fab134bf9a6b15a6142b68dc
Ivanka-karaim <82397773+Ivanka-karaim@users.noreply.github.com>
2022-06-19 00:48:25 +0300

    commit

--- weatherproject/graphics.py
+++ weatherproject/graphics.py
@@ -7,25 +7,41 @@ from windrose import WindroseAxes
 
 my_dict = {"Західний":180, "Південний": 270, "Змінний":0, "Північний":90,
            "Східний":0, "Пд-Зх":225, "Пд-Сх":315, "Пн-Зх":135,"Пн-Сх":45, "Штиль":0}
+
+
+def autolabel(rects, labels=None, height_factor=1.01, ax=None):
+    for i, rect in enumerate(rects):
+        height = rect.get_height()
+        if labels is not None:
+            try:
+                label = labels[i]
+            except (TypeError, KeyError):
+                label = ' '
+        else:
+            label = '%d' % int(height)
+        plt.gca().text(rect.get_x() + rect.get_width()/2., height_factor*height,
+                '{}'.format(label),
+                ha='center', va='bottom')
+
+
 def graphics_2(city, date_1, date_2):
+    # змінити на зелений колір, забрати вісь ігрик, і якщо вісь забереться то трошки змінити назву
     sql = "SELECT T, count(*) FROM data_{:} WHERE date_time>={:} and date_time<={:} GROUP BY T ORDER BY T ;".format(city, date_1, date_2)
     connector = sqlite3.connect("db.sqlite3")
     s = connector.execute(sql).fetchall()
     s = np.array(s)
-    data_t = []
-    data_c = []
-    # print(s)
-    # data=0
-    # for s_1 in s:
-    #     data_t.append(s_1[0])
-    #     data_c.append(s_1[1]/2)
-    #     data += s_1[1]/2
-    # print(data)
-    plt.bar(s[:, 0], s[:, 1]/2)
+    plt.title('Тривалість температурних режимів')
+    plt.xlabel('Температура, С')
+    plt.ylabel('Час, год')
+    plt.bar(s[:, 0], s[:, 1]/4)
+    plt.xticks(np.arange(min(s[:, 0]), max(s[:,0])+1, step=1))
+    ax = plt.gca()
+    autolabel(ax.patches, s[:,1] / 4, height_factor=1.01, )
     plt.show()
 
 
 def graphics_4(city, date_1, date_2):
+    # додати сетку в один бік(не обовязково)
     sql = "SELECT FF, count(*) FROM data_{:} WHERE date_time>={:} and date_time<={:} GROUP BY FF ORDER BY FF;".format(
         city, date_1, date_2)
     connector = sqlite3.connect("db.sqlite3")
@@ -34,21 +50,27 @@ def graphics_4(city, date_1, date_2):
     data_t = []
     data_c = []
     print(s)
-
-    plt.bar(s[:, 0], s[:, 1]/2)
+    plt.title('Тривалість режимів вітрової активності')
+    plt.xlabel('Швидкість, м/с')
+    plt.ylabel('Час, год')
+    plt.xticks(np.arange(min(s[:, 0]), max(s[:, 0])+1, step=1))
+    ax=plt.gca()
+    ax.bar(s[:, 0], s[:, 1]/4)
+    # plt.grid(True)
     plt.show()
 
 
 def graphics_1(city, date_1, date_2):
+    # розвернути підписи, і придумати щось з кроком в осі ікс(якщо багато значень, то нічого видно не буде, можна зробити крок, або діставати з бд по днях)
     sql = "SELECT T, date_time FROM data_{:} WHERE date_time>={:} and date_time<={:} ORDER BY date_time;".format(
         city, date_1, date_2)
     connector = sqlite3.connect("db.sqlite3")
     s = connector.execute(sql).fetchall()
     data = np.array(s)
-
-    # plt.scatter( data[:, 1], data[:,0])
-    # plt.show()
-    print(data[:, 1])
+    plt.title('Температурні умови регіону')
+    plt.xlabel('Дата і час')
+    plt.ylabel('Температура, С')
+    plt.grid(True)
     plt.plot(data[:, 1], data[:,0])
     plt.show()
 
@@ -59,18 +81,11 @@ def graphics_3(city, date_1, date_2):
     connector = sqlite3.connect("db.sqlite3")
     s = connector.execute(sql).fetchall()
 
-    data = np.array(s)
-    # print(s)
-    # for d in data:
-    #     d[1]=int(my_dict[d[1]])
-    # print(data)
-    print(data[:,1])
-    ws = np.random.random(96) * 6
-    wd = np.random.random(500) * 360
     ax = WindroseAxes.from_ax()
-    print(len(data[:,1]))
+
     data_t = []
     data_c=[]
+    plt.title('Троянда вітрів')
     for s_1 in s:
         data_t.append(s_1[0])
         data_c.append(my_dict[s_1[1]])
@@ -79,10 +94,11 @@ def graphics_3(city, date_1, date_2):
     ax.set_legend()
     plt.savefig("demo.png")
 
+
 # graphics_2("kyiv", "'2012-01-01 00:00'", "'2012-01-01 23:59'")
 # graphics_4("kyiv", "'2012-01-01 00:00'", "'2012-01-01 23:59'")
 # graphics_1("kyiv", "'2012-01-01 00:00'", "'2012-01-01 23:59'")
-graphics_3("kyiv", "'2012-01-01 00:00'", "'2012-01-04 23:59'")
+# graphics_3("kyiv", "'2012-01-01 00:00'", "'2012-01-04 23:59'")
 
 
 
